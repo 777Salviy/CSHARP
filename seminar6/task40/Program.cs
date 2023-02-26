7f7944133ceae0bf6d2f3d574b8364b72bb313e6
777salviy@gmail.com <777salviy@gmail.com>
2023-02-26 14:48:51 +0300

    add task40 solution

--- seminar6/task40/Program.cs
+++ seminar6/task40/Program.cs
@@ -3,3 +3,23 @@
 // Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы 
 // двух других сторон.
 
+int Vvod(string msg)
+{
+  Console.WriteLine(msg);
+  int num = Convert.ToInt32(Console.ReadLine());
+  return num;
+}
+
+bool Rectangle(int a, int b, int c)
+{
+  bool res = false;
+  if (a < b + c & b < a + c & c < a + b)
+  res = true;
+  return res;
+}
+
+int a = Vvod("Введите длину первой стороны треугольника: ");
+int b = Vvod("Введите длину второй стороны треугольника: ");
+int c = Vvod("Введите длину третьей стороны треугольника: ");
+
+Console.WriteLine(Rectangle(a,b,c));
