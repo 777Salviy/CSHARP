f1bde3614c2769b370dbb7227bfdf3d2e8b17ba0
777salviy@gmail.com <777salviy@gmail.com>
2023-02-26 14:54:18 +0300

    add task 42 solution

--- seminar6/task42/Program.cs
+++ seminar6/task42/Program.cs
@@ -3,4 +3,25 @@
 // 3  -> 11
 // 2  -> 10
 
+int Vvod(string msg)
+{
+  Console.WriteLine(msg);
+  int num = Convert.ToInt32(Console.ReadLine());
+  return num;
+}
 
+void Conv (int num)
+{
+  int num1 = 0;
+  string s = "";
+    while (num > 0)
+  {
+    num1 = num%2;
+    num = num/2;
+    s = num1 + s;
+  }
+   Console.WriteLine(s);
+}
+
+int number = Vvod("Введите число: ");
+Conv(number);
