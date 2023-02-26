20499bf972fb2cadeb5a4000904dd34d7e896db0
777salviy@gmail.com <777salviy@gmail.com>
2023-02-26 14:32:45 +0300

    add task39 solution

--- seminar6/task39/Program.cs
+++ seminar6/task39/Program.cs
@@ -5,3 +5,34 @@
 // [6 7 3 6] -> [6 3 7 6]
 
 
+void FillArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 100);
+}
+
+void PrintArray(int[] array)
+{
+  foreach (var el in array)
+    Console.Write($"{el} ");
+  Console.WriteLine();
+}
+
+void Reverse(int[] array)
+{
+    int temp;
+    int size = array.Length;
+    for (int i=0;i<size/2;i++)
+        {
+            // temp = array[i];
+            // array[i]=array[size-1-i];
+            // array[size-1-i] = temp;
+            (array[i], array[size-1-i]) = (array[size-1-i], array[i]);
+        }
+}
+
+int[] array = new int [7];
+FillArray(array);
+PrintArray(array);
+Reverse(array);
+PrintArray(array);
