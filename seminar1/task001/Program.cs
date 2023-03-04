8da407a9466b2814c2e696cd6d3b8d86cd237270
777salviy@gmail.com <777salviy@gmail.com>
2023-03-04 22:48:00 +0300

    add task001 checking 9 equal

--- seminar1/task001/Program.cs
+++ seminar1/task001/Program.cs
@@ -10,3 +10,16 @@ Console.WriteLine ("Введите число: ");
 int i = Convert.ToInt32(Console.ReadLine ());
 int num = i*i;
 Console.WriteLine (num);
+
+if (num>9)
+{
+    Console.WriteLine("Число больше 9");
+}
+else
+{
+    Console.WriteLine("Число меньше 9");
+}
+if (num == 9)
+{
+    Console.WriteLine("Число равно 9");
+}
