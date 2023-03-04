8e4a50d3da5353b9fcc18868674d8984bf671c26
777salviy@gmail.com <777salviy@gmail.com>
2023-03-04 14:42:19 +0300

    add example007 triangle through 3 points

--- lection1/example007/Program.cs
+++ lection1/example007/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear ();
+// Console.SetCursorPosition (10, 4);
+// Console.WriteLine ("+");
+
+int xa = 35, ya = 1,
+    xb = 1, yb = 20,
+    xc = 70, yc = 20;
+
+Console.SetCursorPosition (xa, ya);
+Console.WriteLine ("+");
+
+Console.SetCursorPosition (xb, yb);
+Console.WriteLine ("+");
+
+Console.SetCursorPosition (xc, yc);
+Console.WriteLine ("+");
+
+
+int x = xa, y = xb;
+int count = 0;
+
+while (count < 10)
+{
+    int what = new Random().Next (0, 3);
+}
