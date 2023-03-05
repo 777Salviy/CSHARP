5c89b34a33e2b05441c2daa863f63246f107aea3
777salviy@gmail.com <777salviy@gmail.com>
2023-03-05 17:29:17 +0300

    add task05 solution

--- seminar1/task05/Program.cs
+++ seminar1/task05/Program.cs
@@ -7,3 +7,9 @@
 // Console.WriteLine ("Введите целое число: ");
 // int number = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("номер");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = -number; i <= number; i++)
+{
+  Console.Write(i + " ");
+}
