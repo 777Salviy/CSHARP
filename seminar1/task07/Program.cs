847061fd5dfa5dbe01d64f651a3629af51c817b5
777salviy@gmail.com <777salviy@gmail.com>
2023-03-05 19:41:25 +0300

    add task07 solution two types

--- seminar1/task07/Program.cs
+++ seminar1/task07/Program.cs
@@ -3,3 +3,23 @@
 //пример: 456 -> 6
 
 
+// Console.Write("Введите трехзначное число: ");
+// int number = int.Parse(Console.ReadLine());
+// int lastDigit = number % 10; 
+// Console.WriteLine("Последняя цифра: " + lastDigit);
+// Данный вариант не предполагает исключение введенея разнядности более 3.
+
+// другой вариант
+
+Console.WriteLine ("Введите трёхзначное число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+if (number > 99 && number < 1000)
+{
+    int lastnumber = number % 10;
+    System.Console.WriteLine ("Последнее число: " + lastnumber);
+}
+else
+{
+    Console.WriteLine ("Неверное число");
+}
