68a3de9e7ecdbf0bd50a7ea2e3f7ae6d6025d0e0
777salviy@gmail.com <777salviy@gmail.com>
2023-03-06 21:49:40 +0300

    add task08_HW solution

--- seminar1/task08_HW/Program.cs
+++ seminar1/task08_HW/Program.cs
@@ -3,3 +3,14 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+
+}
