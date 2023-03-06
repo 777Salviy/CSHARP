fb93bb0313a26db896ba318d432bd61ff9a06068
777salviy@gmail.com <777salviy@gmail.com>
2023-03-06 17:05:19 +0300

    add task02_HW two types

--- seminar1/task02_HW/Program.cs
+++ seminar1/task02_HW/Program.cs
@@ -4,3 +4,36 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine("Оба числа равны");
+}
+
+else
+{
+    if (number1 < number2)
+    {
+        Console.WriteLine("Max = " + number2);
+    }
+    else
+    {
+        Console.WriteLine("Max = " + number1);
+    }
+}
+
+// Console.WriteLine("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int max = number1;
+
+// if (number2 > max) max = number2; 
+
+// Console.Write ("max = ");
+// Console.WriteLine (max);
